4dc4933bbe080282d45617cb4bb815c49dbcb98d
Chi Luu <35760570+geez2k@users.noreply.github.com>
2018-06-12 12:14:43 -0600

    Service Worker adjustment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter } from 'react-router-dom';
 
 /* const getCurrentDate = () => {
@@ -13,3 +14,4 @@ const greeting = <h1>Hello World!!! Current date: {getCurrentDate()}</h1>; */
 ReactDOM.render(
     <BrowserRouter><App /></BrowserRouter>, 
     document.getElementById('root'));
+registerServiceWorker();
